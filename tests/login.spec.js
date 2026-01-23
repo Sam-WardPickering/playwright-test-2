6d3f0d2d75e7864c5d7bcf9b23ddce5ebda7b757
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-01-23 14:38:50 +1300

    Add comments, validate logout state

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -3,6 +3,7 @@ import { test, expect } from '@playwright/test';
 test.only('Login test', async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/login');
 
+    // Login
     await page.locator('#username').click();
     await page.locator('#username').fill('tomsmith');
 
@@ -11,10 +12,19 @@ test.only('Login test', async ({ page }) => {
 
     await page.getByRole('button', { name: /login/i }).click();
 
+    // Validate login
     await expect(page.getByText('You logged into a secure area')).toBeVisible();
     await expect(page.locator('h4.subheader')).toContainText('Welcome to the Secure Area. When you are done click logout below.');
 
     await page.pause();
-    
 
+    // Logout
+    await page.locator('a.button.second.radius:has-text("Logout")').click();
+
+    // Validate logout
+    await page.getByLabel('Username').click();
+    await page.getByRole('textbox', { name: 'Username' }).fill('11');
+    await expect(page.getByRole('textbox', { name: 'Username' })).toHaveValue('11');
+
+    await page.pause();
 });
