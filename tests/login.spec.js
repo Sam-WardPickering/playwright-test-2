4ca78783f7e3f7d7770a8cf84e3f0c74f8a65624
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-01-23 14:09:31 +1300

    Get username field

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -2,4 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('Login test', async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/login');
+    await page.locator('#username').click();
 });
