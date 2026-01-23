1202bf495d100d90f0644ee10d3b158d6ac267aa
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-01-23 15:16:48 +1300

    Create basic function to increment count value, add script to html

--- clickMe.js
+++ clickMe.js
@@ -1,1 +1,9 @@
 let count = 0;
+const button = document.getElementById("clickButton");
+const counter = document.getElementById("counter");
+
+button.addEventListener("click", incrementCount);
+
+function incrementCount() {
+    counter.textContent = ++count;
+}
