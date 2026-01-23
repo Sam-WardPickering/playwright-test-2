476ffaaa91ade79fbfc5e946cc690c4f2e39a6e2
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-01-23 14:22:09 +1300

    Log into page

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,6 +1,17 @@
 import { test, expect } from '@playwright/test';
 
-test('Login test', async ({ page }) => {
+test.only('Login test', async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/login');
+
     await page.locator('#username').click();
+    await page.locator('#username').fill('tomsmith');
+
+    await page.locator('#password').click();
+    await page.locator('#password').fill('SuperSecretPassword!');
+
+    await page.getByRole('button', { name: /login/i }).click();
+
+    await page.pause();
+    
+
 });
