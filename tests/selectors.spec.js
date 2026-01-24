c53abdebfd4be1b332e9807b612f48d7a3256a2a
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-01-25 11:08:28 +1300

    Refactor test:

--- tests/selectors.spec.js
+++ tests/selectors.spec.js
@@ -3,17 +3,26 @@
 import { test, expect } from '@playwright/test';
 
 test.only("Using selectors", async ({ page }) => {
+    let clickCount = 0;
+
     await page.goto('http://127.0.0.1:5500/clickMe.html')
 
     // By ID
     await page.locator("#clickButton").click();
+    ++clickCount;
 
-    //By class
+    //By Class
     await page.locator(".button-style").click();
+    ++clickCount;
 
-    //By tag and class
+    //By Tag and Class
     await page.locator("button.button-style").click();
+    ++clickCount;
 
-    await expect(page.locator("#counter")).toContainText("3");
+    // By Attribute Value
+    await page.locator('[data-action="increment"]').click();
+    ++clickCount;
+
+    await expect(page.locator("#counter")).toHaveText("4");
 
 });
