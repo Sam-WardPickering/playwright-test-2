a1ef6a09d0d69d568d36d804c226d5c57c5100ee
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-01-25 11:10:15 +1300

    Remove variable used to capture clicks

--- tests/selectors.spec.js
+++ tests/selectors.spec.js
@@ -3,25 +3,19 @@
 import { test, expect } from '@playwright/test';
 
 test.only("Using selectors", async ({ page }) => {
-    let clickCount = 0;
-
     await page.goto('http://127.0.0.1:5500/clickMe.html')
 
     // By ID
     await page.locator("#clickButton").click();
-    ++clickCount;
 
     //By Class
     await page.locator(".button-style").click();
-    ++clickCount;
 
     //By Tag and Class
     await page.locator("button.button-style").click();
-    ++clickCount;
 
     // By Attribute Value
     await page.locator('[data-action="increment"]').click();
-    ++clickCount;
 
     await expect(page.locator("#counter")).toHaveText("4");
 
