f0da90499f49e2346706805ecb424897387c98f4
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-01-25 17:08:46 +1300

    Add second test

--- tests/assertions.spec.js
+++ tests/assertions.spec.js
@@ -4,5 +4,17 @@ test.describe.only('Practicing Assertions', async () => {
 
     test('Verify web page behaviour', async ({ page }) => {
         await page.goto('https://the-internet.herokuapp.com/');
+
+        // Assert URL
+        await expect(page).toHaveURL('https://the-internet.herokuapp.com/');
+
+        // Assert title
+        await expect(page).toHaveTitle('The Internet');
+
+    });
+
+    test.only('Continue with assertions', async ({ page }) => {
+        // Assert visibility
+        await expect(page.locator('h1')).toBeVisible();
     });
 });
