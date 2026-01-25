040a24b55ced3069c99ee96efa8c6d2961738b6d
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-01-25 17:00:29 +1300

    Create new file for practicing assertions and set up initial test

---
+++ tests/assertions.spec.js
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test.describe.only("Practicing Assertions", async ({ page }) => {
+
+    test("Verify web page behaviour", async ({ page }) => {
+
+    });
+});
