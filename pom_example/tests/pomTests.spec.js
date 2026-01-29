76acddea58985de174937ec9914e76109c6f6023
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-01-30 10:09:51 +1300

    Comment out unnecessary code:

--- pom_example/tests/pomTests.spec.js
+++ pom_example/tests/pomTests.spec.js
@@ -7,6 +7,10 @@ test.describe('Login Tests', () => {
     test.beforeEach(async ({ page }) => {
         pm = new PomManager(page);
     });
+
+    // test.afterEach(async ({ page }) => {
+    //     await page.close();
+    // });
     
     test('Login with valid credentials', async () => {
         await pm.loginPage.navigate();
