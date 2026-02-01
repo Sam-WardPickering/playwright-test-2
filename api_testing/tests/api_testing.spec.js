9ad9f1229b7fdb4a8d2eeec5b173a40275671b7d
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-02-02 10:20:21 +1300

    Update api key

--- api_testing/tests/api_testing.spec.js
+++ api_testing/tests/api_testing.spec.js
@@ -5,7 +5,7 @@ test.describe('API verification examples', () => {
     // Test to verify users endpoint is returning expected user data
     test('Verify multi records against stored static response', async ({ request }) => {
         // Save raw response into a variable
-        const response = await request.get('https://reqres.in/api/users');
+        const response = await request.get('https://reqres.in/api/users', { headers: {'x-api-key': 'reqres_f477729486b947269579c7b1c056f338'}});
 
         // Parse the response body into JS object with access to the actual data within the response body
         const responseBody = await response.json();
