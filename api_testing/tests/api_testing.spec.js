844b8cb11fd306f418a8334f615510cf675a739d
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-02-03 10:42:42 +1300

    Create test for verifying POST request

--- api_testing/tests/api_testing.spec.js
+++ api_testing/tests/api_testing.spec.js
@@ -19,7 +19,7 @@ test.describe('API verification examples', () => {
     })
 
     // 2) Test daya for a single user line by line
-    test.only('Verify single user line by line', async ({ request }) => {
+    test('Verify single user line by line', async ({ request }) => {
         const response = await request.get('https://reqres.in/api/users/1', { headers: {'x-api-key': 'reqres_f477729486b947269579c7b1c056f338'}});
 
         const responseBody = await response.json();
@@ -32,5 +32,10 @@ test.describe('API verification examples', () => {
         expect(responseBody.data.first_name).toBe('George');
         expect(responseBody.data.last_name).toBe('Bluth');
         expect(responseBody.data.avatar).toBe('https://reqres.in/img/faces/1-image.jpg');
+    });
+
+    // 3) Test POST request
+    test.only('Verify POST request', async ({ request }) => {
+
     });
 });
